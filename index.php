27638df10bef3df6c2a64d7fc0ed51d054de7ef1
Bau Huynh <bau.hq@neo-lab.vn>
2023-02-24 10:51:56 +0700

    Done videojs

--- index.php
+++ index.php
@@ -2,20 +2,35 @@
 <html>
 
 <head>
-    <link href="//vjs.zencdn.net/7.15.4/video-js.min.css" rel="stylesheet">
-    <script src="//vjs.zencdn.net/7.15.4/video.min.js"></script>
+    <link href="/videojs/dist/video-js.min.css" rel="stylesheet">
+    <script src="/videojs/dist/video.min.js"></script>
+    <link href="/videojs-quality-selector/dist/css/quality-selector.css" rel="stylesheet">
+    <script src="/videojs-quality-selector/dist/js/silvermine-videojs-quality-selector.min.js"></script>
 </head>
 
 <body>
     <video id="my-video" class="video-js" controls preload="auto" width="1280" height="720">
-        <source src="/movie.php?filename=output.m3u8" type="application/x-mpegURL">
+        <source src="/movie.php?filename=output.m3u8&quality=720p" type="application/x-mpegURL" label="720p">
+        <source src="/movie.php?filename=output.m3u8&quality=360p" type="application/x-mpegURL" label="360p">
         <p class="vjs-no-js">
             To view this video please enable JavaScript, and consider upgrading to a
             web browser that supports HTML5 video
         </p>
     </video>
     <script>
-        var player = videojs('my-video');
+        options = {
+            controlBar: {
+                children: [
+                    'playToggle',
+                    'progressControl',
+                    'volumePanel',
+                    'qualitySelector',
+                    'fullscreenToggle',
+                ],
+            },
+        };
+        var player = videojs('my-video', options);
+        player.controlBar.addChild('QualitySelector');
     </script>
 </body>
 
--- movie.php
+++ movie.php
@@ -1,10 +1,11 @@
 <?php
 
 $filename = $_GET['filename'];
+$quality = $_GET['quality'];
 $ext = pathinfo($filename, PATHINFO_EXTENSION);
 if ($ext === 'm3u8') {
     header('Content-Type: application/vnd.apple.mpegurl');
 }
 
-$path = __DIR__ . '/example/' . $filename;
+$path = __DIR__ . "/example/$quality/$filename";
 echo file_get_contents($path);
