bbb3bd6f4dc4d3eac24dcfe0c6ad4b9520fdf4d0
Bau Huynh <bau.hq@neo-lab.vn>
2023-02-24 11:12:20 +0700

    Add sub

--- index.php
+++ index.php
@@ -6,12 +6,18 @@
     <script src="/videojs/dist/video.min.js"></script>
     <link href="/videojs-quality-selector/dist/css/quality-selector.css" rel="stylesheet">
     <script src="/videojs-quality-selector/dist/js/silvermine-videojs-quality-selector.min.js"></script>
+    <style>
+        .vjs-menu li {
+            text-transform: initial;
+        }
+    </style>
 </head>
 
 <body>
     <video id="my-video" class="video-js" controls preload="auto" width="1280" height="720">
-        <source src="/movie.php?filename=output.m3u8&quality=720p" type="application/x-mpegURL" label="720p">
-        <source src="/movie.php?filename=output.m3u8&quality=360p" type="application/x-mpegURL" label="360p">
+        <source src="/movie.php?filename=output.m3u8&quality=720p" type="application/x-mpegURL" label="HD">
+        <source src="/movie.php?filename=output.m3u8&quality=360p" type="application/x-mpegURL" label="SD">
+        <track label="English" kind="subtitles" srclang="en" src="/sub.vtt" default />
         <p class="vjs-no-js">
             To view this video please enable JavaScript, and consider upgrading to a
             web browser that supports HTML5 video
@@ -19,18 +25,19 @@
     </video>
     <script>
         options = {
+            errorDisplay: false,
             controlBar: {
                 children: [
                     'playToggle',
-                    'progressControl',
                     'volumePanel',
+                    'progressControl',
+                    'subtitlesButton',
                     'qualitySelector',
-                    'fullscreenToggle',
-                ],
-            },
+                    'fullscreenToggle'
+                ]
+            }
         };
         var player = videojs('my-video', options);
-        player.controlBar.addChild('QualitySelector');
     </script>
 </body>
 
